1cbe93e58a34d99a94f6b69fb5a6f7044bc36396
meggmumbi <margaretmumbi16@gmail.com>
2024-05-08 15:11:46 +0300

    Added Programme type

--- SCIT/Entities/Programmes.cs
+++ SCIT/Entities/Programmes.cs
@@ -4,9 +4,11 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+
+        public string ProgrammeType { get; set; }
         public string Description { get; set; }
         public Guid DepartmentId { get; set; }
-        public Guid StaffId { get; set; }
+        public Guid? StaffId { get; set; }
 
     }
 }
---
+++ SCIT/Migrations/20240508100135__programTypeAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SCIT.Migrations
+{
+    /// <inheritdoc />
+    public partial class _programTypeAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProgrammeType",
+                table: "Programmes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProgrammeType",
+                table: "Programmes");
+        }
+    }
+}
